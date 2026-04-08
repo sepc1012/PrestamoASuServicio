b53575289a4f7145a37fe19e47ab318d32611a87
sepc1012 <pizarrochauca.sebastian@gmail.com>
2026-04-08 13:23:23 -0500

    cambio de url

--- src/main/java/com/prestamosasuservicio/backend/config/SecurityConfig.java
+++ src/main/java/com/prestamosasuservicio/backend/config/SecurityConfig.java
@@ -53,7 +53,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "https://loving-adaptation-production-658f.up.railway.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
